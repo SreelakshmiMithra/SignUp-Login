bf8b2906ec2acc3c01ebc4e0ebab4afc5ea5f897
SreelakshmiMithra <lakshmi.badiginchala23@gmail.com>
2025-11-14 08:07:20 +0530

    login feature added

--- index.js
+++ index.js
@@ -11,7 +11,21 @@ function signup(userName) {
     return "Signup Successful, Please Login";
   }
 }
+// Login function
+function login(userName, password) {
+  let users = ["Lakshmi", "Ravi", "Sita", "Kiran"];
+
+  if (!users.includes(userName)) {
+    return "User Not Found, Please Signup";
+  } else if (password !== "Emp@123") {
+    return "Wrong Password....";
+  } else {
+    return "Login Successful...";
+  }
+}
 
 // test
-console.log(signup("Lakshmi")); // User Already Registered, Please Login
-console.log(signup("Anjali"));  // Signup Successful, Please Login
+console.log(signup("Anjali"));
+console.log(login("Lakshmi", "Emp@123"));
+console.log(login("Sita", "wrongpass"));
+console.log(login("Unknown", "Emp@123"));
